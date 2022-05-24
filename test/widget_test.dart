14b0c0baf8cc3d89e276780d29e6b32f3fbfe12b
Ahmed Gaafar <a.gaafar.junior@gmail.com>
2022-05-24 07:55:04 +0300

    removing import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,8 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:focus/main.dart';
-
 void main() {
   testWidgets('checking click', (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(
